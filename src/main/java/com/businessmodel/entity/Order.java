1abae709b5c41b366283f07a991daec9b41f6bd8
Vaduni Niranjan <vaduniniranjan@gmail.com>
2026-03-25 16:42:25 +0530

    Added Order Entity class

---
+++ src/main/java/com/businessmodel/entity/Order.java
@@ -0,0 +1,114 @@
+package com.businessmodel.entity;
+
+import jakarta.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @Column(name = "orderNumber")
+    private Integer orderNumber;
+
+    @Column(name = "orderDate")
+    private Date orderDate;
+
+    @Column(name = "requiredDate")
+    private Date requiredDate;
+
+    @Column(name = "shippedDate")
+    private Date shippedDate;
+
+    private String status;
+
+    private String comments;
+    
+    @ManyToOne
+    @JoinColumn(name = "customerNumber")
+    private Customer customer;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderDetail> orderDetails;
+   
+    public Order() {}
+    
+	public Order(Integer orderNumber, Date orderDate, Date requiredDate, Date shippedDate, String status,
+			String comments, Customer customer, List<OrderDetail> orderDetails) {
+		super();
+		this.orderNumber = orderNumber;
+		this.orderDate = orderDate;
+		this.requiredDate = requiredDate;
+		this.shippedDate = shippedDate;
+		this.status = status;
+		this.comments = comments;
+		this.customer = customer;
+		this.orderDetails = orderDetails;
+	}
+
+	public Integer getOrderNumber() {
+		return orderNumber;
+	}
+
+	public void setOrderNumber(Integer orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+
+	public Date getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public Date getRequiredDate() {
+		return requiredDate;
+	}
+
+	public void setRequiredDate(Date requiredDate) {
+		this.requiredDate = requiredDate;
+	}
+
+	public Date getShippedDate() {
+		return shippedDate;
+	}
+
+	public void setShippedDate(Date shippedDate) {
+		this.shippedDate = shippedDate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public List<OrderDetail> getOrderDetails() {
+		return orderDetails;
+	}
+
+	public void setOrderDetails(List<OrderDetail> orderDetails) {
+		this.orderDetails = orderDetails;
+	}
+  
+}
