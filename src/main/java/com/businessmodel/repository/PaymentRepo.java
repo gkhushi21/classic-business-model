b54cdad1f25ee7f04b699b6d8360f3e837fe1348
Khushi Gupta <gkhushi2901@gmail.com>
2026-03-29 02:09:23 +0530

    added payment api method

--- src/main/java/com/businessmodel/repository/PaymentRepo.java
+++ src/main/java/com/businessmodel/repository/PaymentRepo.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.businessmodel.entity.Payment;
 import com.businessmodel.entity.PaymentId;
 
+
 public interface PaymentRepo  extends JpaRepository<Payment, PaymentId> {
 
 }
