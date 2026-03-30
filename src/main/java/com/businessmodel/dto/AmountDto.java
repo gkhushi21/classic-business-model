5e5ac94589bae611f3d03826fc02dc0779917db9
Khushi Gupta <gkhushi2901@gmail.com>
2026-03-30 11:29:34 +0530

    Updated AmountDto

--- src/main/java/com/businessmodel/dto/AmountDto.java
+++ src/main/java/com/businessmodel/dto/AmountDto.java
@@ -1,6 +1,27 @@
 package com.businessmodel.dto;
 
-public class AmountDto {
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AmountDto {
+	@NotNull
+	private Integer customerNumber;
+	@NotBlank
+	private String checkNumber;
+	@NotNull
+	private LocalDate paymentDate;
+	@NotNull
+	private BigDecimal amount;
+
+	private BigDecimal totalAmount;
 
 }
